967f9c6917c6c1e899d9c023b4d6ef4ee1e356e5
Marco Mechini <mechinimarco35@gmail.com>
2025-04-16 15:07:51 +0200

    correzione esercizio

--- src/main.ts
+++ src/main.ts
@@ -7,92 +7,112 @@ type Person ={
   image: string
 }
 
+type ActressNationality = 
+| 'American' 
+| 'British' 
+| 'Australian'
+| 'Israeli-American' 
+| 'South African' 
+| 'French' 
+| 'Indian' 
+| 'Israeli' 
+| 'Spanish' 
+| 'South Korean' 
+| 'Chinese'
+
 type Actress = Person & {
   most_famous_movies : [string, string, string]
   awards: string,
-  nationality : 'American' | 'British' | 'Australian' | 'Israeli-American' | 'South African' | 'French' | 'Indian' | 'Israeli' | 'Spanish' | 'South Korean' | 'Chinese'
+  nationality : ActressNationality
 }
 
-function isActress<Actress>(user:  Actress){
-  if (user && 
-    typeof user ==='object' &&
-    "id" in user && 
-    typeof user.id === 'number' &&
-    "name" in user && 
-    typeof user.name === 'string' &&
-    "birth_year" in user && 
-    typeof user.birth_year === 'number' &&
-    "biography" in user && 
-    typeof user.biography === 'string' &&
-    "image" in user && 
-    typeof user.image === 'string' &&
+function isActress(user:  unknown) : user is Actress{
+  return (
+    typeof user ==='object' && user !== null &&
+    "id" in user && typeof user.id === 'number' &&
+    "name" in user && typeof user.name === 'string' &&
+    "birth_year" in user && typeof user.birth_year === 'number' &&
+    "biography" in user && typeof user.biography === 'string' &&
+    "image" in user && typeof user.image === 'string' &&
     "most_famous_movies" in user && 
-    Array.isArray(user.most_famous_movies) &&
-    "awards" in user && 
-    typeof user.awards === 'string' &&
-    "nationality" in user && 
-    typeof user.nationality === 'string'
-  ) {
-    return true
-  }
-  return false
-}
+    user.most_famous_movies instanceof Array &&
+    user.most_famous_movies.length === 3 &&
+    user.most_famous_movies.every(m => typeof m === 'string') &&
+    "awards" in user && typeof user.awards === 'string' &&
+    "nationality" in user && typeof user.nationality === 'string'
+  ) 
+} 
 
-async function getAccress(id: number){
+async function getAccress(id: number): Promise<Actress | null>{
   try {
     const response =  await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/actresses/${id}`)
-    if (!response.ok) {
+    const dataJson : unknown = await response.json()
+    if (!isActress(dataJson)) {
       throw new Error(`Errore nella richiesta HTTP`);
     }
-  const dataJson : unknown = await response.json()
-  if (isActress(dataJson)) {
-    return dataJson
-  }
-  return null
+    return dataJson;
 
   } catch (error) {
-    console.error(error);
-    
+    if (error instanceof Error){
+      console.error('Errore durante il recuper dell\'attrice:', error);
+    }
+    else {
+      console.error('Errore sconosciuto:',error);
+    }
+    return null
   }
 }
 
-async function getAllActresses (){
+async function getAllActresses (): Promise<Actress[]>{
   try {
     const response =  await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/actresses`)
     if (!response.ok) {
-      throw new Error(`Errore nella richiesta HTTP`);
+      throw new Error(`Errore nella richiesta HTTP ${response.status}`);
     }
     const dataJson : unknown = await response.json()
-    if (Array.isArray(dataJson)) {
-      console.log(dataJson);
-      return dataJson
+    if (!Array.isArray(dataJson)) {
+      throw new Error(`Formato dei dati non valido non è un array`);
     }
+    const attriciValide: Actress[] = dataJson.filter(d => isActress(d))
+    return attriciValide
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error){
+      console.error('Errore durante il recuper dell\'attrice:', error);
+    }
+    else {
+      console.error('Errore sconosciuto:',error);
+    }
+    return []
   }
 }
 
-async function getActresses (ids: number[]) {
+async function getActresses (ids: number[]): Promise<(Actress | null)[]> {
 
   try {
-    const allIds = ids.map(async (id) => {
-      const response =  await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/actresses/${id}`)
-      if (!response.ok) {
-        return null
-      }
-      const dataJson : unknown = await response.json()
-      if (isActress(dataJson)) {
-        return dataJson
-      }
-    })
-    console.log('Promis');
-    return await Promise.allSettled(allIds).then(obj => console.log(obj))
+    const allIds = ids.map((id) => getAccress(id))
+    return await Promise.all(allIds)
 
   } catch (error) {
-    console.error(error);
-    
+    if (error instanceof Error){
+      console.error('Errore durante il recuper dell\'attrice:', error);
+    }
+    else {
+      console.error('Errore sconosciuto:',error);
+    }
+    return []
   }
 }
 
-let numeri: number[] = [...Array(50).keys()].map((i) => i + 1);
-getActresses(numeri)
+function createActress(data: Omit<Actress, 'id'>): Actress{
+  return {
+    ...data,
+    id: Math.floor(Math.random() * 10000)
+  }
+}
+
+function updateActress(actress: Actress, updates: Partial< Omit<Actress, 'id'|'name'>>){
+  return {
+    ...actress,
+    ...updates,
+  }
+}
